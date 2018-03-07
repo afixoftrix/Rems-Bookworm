80ec3fdafd75880ff63af9271544bc5589886268
Unwana Essien <unwana.essien@outlook.com>
2018-03-07 12:08:32 +0100

    app now searches db for user creds and reroutes on success

--- bookworm-api/src/index.js
+++ bookworm-api/src/index.js
@@ -1,12 +1,17 @@
 import express from 'express';
 import path from 'path'
-import { log } from 'util';
+import mongoose from 'mongoose'
+import bodyParser from 'body-parser'
+import auth from './routes/auth'
+
+// passwordhash for pass 'abc': $2a$10$1zSye2oiueM6v9Xq9x4nVOGY1bKXzNuPCn1f1HcJhLE./7ev6hWhm
+// passwordhash for pass 'lol': $2a$10$eaa15IlRRzOt6hMKP0D7wOBOnvn6IXvcA7B5GJjmSWl8b.eLVzOje
 
 const app = express();
-
-app.post('/api/auth', (req, res) => {
-    res.status(400).json({errors: { global: ("Invalid Credentials")}})
-})
+app.use(bodyParser.json())
+mongoose.connect('mongodb://localhost/bookworm');
+// , { useMongoClient: true }
+app.use('/api/auth', auth)
 
 app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
